5e6c9b7baa5774af98585401246fb03f0ba3f30c
Atumah Daniel <atumahdaniel@gmail.com>
2021-03-30 19:01:42 +0100

    test commit

---
+++ background.js
@@ -0,0 +1,9 @@
+let color = "#3aa757";
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log("Default background color set to %cgreen", `color: ${color}`);
+});
+
+
+// By adding %c to green you are formatting the text to take in the green color specified in the green variable
